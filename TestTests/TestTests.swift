97ba81ac8b4f0d29a0ca8d2947c724147c9265f3
pasin7793 <pasin7793@naver.com>
2021-12-15 16:10:59 +0900

    test

--- TestTests/TestTests.swift
+++ TestTests/TestTests.swift
@@ -16,6 +16,7 @@ class TestTests: XCTestCase {
 
     override func tearDownWithError() throws {
         print("asdfg")
+        
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
