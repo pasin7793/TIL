5311aa8d354516617904209ecf4533e51fcfa5e5
pasin7793 <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>
2021-12-15 16:09:20 +0900

    test

--- TestTests/TestTests.swift
+++ TestTests/TestTests.swift
@@ -15,6 +15,7 @@ class TestTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
+        print("asdfg")
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
